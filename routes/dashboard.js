8d0f0c78451d4141e983413f58c7db01d48a69a6
Lucas de Oliveira Pereira <olucas1998@gmail.com>
2019-10-09 11:54:51 -0300

    primeira versão das telas adicionar e editar colaboradores

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,9 +3,24 @@ module.exports = function(app){
     app.get('/', 
     function(req, res){
   
-        res.render('index', { 
+        res.redirect('/dashboard');
+  
+    });
 
+    app.get('/:page/', 
+    function(req, res){
+
+        var getPage = req.params.page;
+        var getAction = req.query.action;
+        var getId = req.query.id;
+        
+        res.render('index', { 
+            getPage : getPage,
+            getAction: getAction
         })
+        
+  
+
   
     });
       
